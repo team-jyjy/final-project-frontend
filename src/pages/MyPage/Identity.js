1fb3f9a01941fba4a67fa001de2c2dae2326e498
yuyeon329 <peachberryjr@gmail.com>
2022-05-14 14:08:05 +0900

    update mypage- myprofile

--- src/pages/MyPage/Identity.js
+++ src/pages/MyPage/Identity.js
@@ -3,16 +3,20 @@ import profile from "../../assets/images/salad.jpg";
 import { Avatar } from '@mui/material';
 import { deepOrange, deepPurple } from '@mui/material/colors';
 import "./Identity.css"
+import item1 from "./../../assets/images/profile.png"
+import {makeStyles, useMediaQuery} from "@material-ui/core";
 
 const Identity = () => {
   return(
     <div className='Container1'>
-      <div>My 프로필</div>
-        <div className='wrapper'>   
+      <div className='wrapper'> 
+      <div className='mypf'>My 프로필</div>
           <div className='inner'>
-            <div className='avater'>
-            <Avatar sx={{ bgcolor: deepOrange[500], width:120, height : 120 }}>사용자</Avatar>
+            <div className='avater {isMobile ? classes.mobile : classes.web}'>
+            <Avatar sx={{ bgcolor: deepPurple[200], width:120, height : 120,
+            width:{sm:110},height:{sm:110}}} src = {item1}></Avatar>
             </div>
+
             <div className='info'>
               <div className='name'>
                 <span id='name'>김국민</span><span>님</span>
@@ -20,13 +24,11 @@ const Identity = () => {
               
               <div className='agesex'>
                 <span className='age'>
-                <span>나이</span><span id='age'>여자</span>
+                <span>나이</span><span id='age'>25</span>
                 </span>
                 <span className='sex'>
                 <span>성별</span><span id='sex'>남자</span>
                 </span>
-                
-                
               </div>
 
               <div className='body'>
