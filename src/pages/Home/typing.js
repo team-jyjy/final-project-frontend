78758c465b37ef238bc1247fe187a404f537c600
yuyeon329 <peachberryjr@gmail.com>
2022-05-18 10:19:23 +0900

    update homepage UI

--- src/pages/Home/typing.js
+++ src/pages/Home/typing.js
@@ -6,7 +6,8 @@ const ReactTypingEffectDemo = () => {
   return (
     <div className='typing'>
       <p>자신과의 싸움,</p>
-      <p>더 이상 지지 않을 당신을</p>
+      <p>더 이상 </p>
+      <p>지지 않을 당신을</p>
         <ReactTypingEffect
           text={["JYGY 합니다."]}
         />
