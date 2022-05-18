a51aeeebe70883b4025b8d8edcb294ca5d90c71d
yuyeon329 <peachberryjr@gmail.com>
2022-05-18 17:35:41 +0900

    update mypage UI

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -100,68 +100,69 @@ const MyPage = () => {
   return (
     <div>
       <div className='Container1'>
-        <div className='wrapper'> 
-          <div className='mypf'>My 프로필</div>
-            <div className='inner'>
-              <div className='avater {isMobile ? classes.mobile : classes.web}'>
-                <Avatar sx={{ marginLeft:'5px', bgcolor: deepPurple[200], width:120, height : 120,
-                  width:{sm:110},height:{sm:110}}} src = {item1}></Avatar>
-                </div>
+        <div className='pfwrapper'> 
+          <div className='mypf1'>My 프로필</div>
+          <div className='inner'>
+            <div className='avatar {isMobile ? classes.mobile : classes.web}'>
+              <Avatar sx={{bgcolor: deepPurple[200], width:120, height : 120,
+                width:{sm:110},height:{sm:110}}} src = {item1}></Avatar>
+              </div>
 
-              <div className='info'>
-                <div className='name'>
-                  <span id='name'>{nickname?nickname:null}</span><span>님</span>
-                </div>
-              
-                <div className='agesex'>
-                  <span className='age'>
-                    <span>나이</span><span id='age'>{age?age:null}</span>
-                  </span>
-                  <span className='sex'>
-                    <span>성별</span><span id='sex'>{sex?sex:null}</span>
-                  </span>
-                </div>
+            <div className='info'>
+              <div className='name'>
+                <span id='name'>{nickname?nickname:null}</span><span>님</span>
+              </div>
+            
+              <div className='agesex'>
+                <span className='age'>
+                  <span>나이</span><span id='age'>{age?age:null}</span>
+                </span>
+                <span className='sex'>
+                  <span>성별</span><span id='sex'>{sex?sex:null}</span>
+                </span>
+              </div>
 
-                <div className='body'>
-                  <span className="height">
-                    <span>키</span><span id='height'>{height?height:null}</span>
-                  </span>
-                  <span className="weight">
-                    <span>몸무게</span><span id='weight'>{weight?weight:null}</span>
-                  </span>
-                </div>
-            </div>
+              <div className='body'>
+                <span className="height">
+                  <span>키</span><span id='height'>{height?height:null}</span>
+                </span>
+                <span className="weight">
+                  <span>몸무게</span><span id='weight'>{weight?weight:null}</span>
+                </span>
+              </div>
+          </div>
           </div>
         </div>
       </div>
       {/* <Finance /> */}
       <div className='Container2'>
-      <div className='finance'>
-      <div className='mypf'>My 금융</div>
-      <div className='inner'>
-        <div className='graph'>
-        <CircularProgress value={successpercent} color={deepPurple[200]} thickness={15} size={90}>
-          <CircularProgressLabel>{successpercent?successpercent:0}%</CircularProgressLabel>
-        </CircularProgress>
-        </div>
-        <div className='ment'>
+        <div className='finance'>
+        <div className='mypf2'>My 금융</div>
+        <div className='inner2'>
+          <div className='graph'>
+          <CircularProgress value={successpercent} color={deepPurple[200]} thickness={15} size={90}>
+          {/* <CircularProgress value={successpercent} color={deepPurple[200]} thickness={15} size={{base:'90', sm:'70', md:'80'}}> */}
+            <CircularProgressLabel>{successpercent?successpercent:0}%</CircularProgressLabel>
+          </CircularProgress>
+          </div>
+          <div className='ment'>
 
-          <div className="username">
-            <span id="username">{nickname?nickname:null}</span><span>님</span>
-          </div>
-          <div className='day'>
-            <span id="day">{successday?successday:null}</span><span>일 더 성공하면</span>
-          </div>
-          <div class="benifit">
-            <span>우대금리를 </span><span id="benefit">{ir?ir:null}</span>% 더
-          </div>
-          <div>
-            <span>적용받을 수 있어요!</span>
+            <div className="username">
+              <span id="username">{nickname?nickname:null}</span><span>님</span>
+            </div>
+            <div className='day'>
+              <span id="day">{successday?successday:null}</span><span>일 더 성공하면</span>
+            </div>
+            <div class="benifit">
+              <span>우대금리를 </span><span id="benefit">{ir?ir:null}</span>% 더
+            </div>
+            <div>
+              <span>적용받을 수 있어요!</span>
+            </div>
           </div>
         </div>
+        </div>
       </div>
-      </div>
-    </div>
     </div>
   );
 };
