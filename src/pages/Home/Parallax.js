9e5230e0e20f07bb613a7eaa1eb6654789b47b3d
yuyeon329 <peachberryjr@gmail.com>
2022-05-19 14:07:14 +0900

    before calendar

--- src/pages/Home/Parallax.js
+++ src/pages/Home/Parallax.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import item1 from "../../assets/images/foodschedule.jpg";
+// import item1 from "../../assets/images/foodschedule.jpg";
 import item2 from "../../assets/images/health.jpg";
 import item3 from "../../assets/images/calendar.jpg";
 import item4 from "../../assets/images/finance.png"
@@ -34,7 +34,7 @@ export default function Parallax() {
         }}
       >
         <div className="copy animate__animated animate__slideInRight animate__slower animate__infinite" 
-        // style={{color:'#5F5A64'}}
+        style={{color:'#5F5A64'}}
         ><p>사진 업로드 한번으로<br></br>칼로리와 <br></br>영양 성분 정보를 <br></br>확인해보세요</p></div>
         </div>
       </div>
@@ -43,12 +43,12 @@ export default function Parallax() {
       <div
         className="bg2"
       >
-        <div className="copy animate__animated animate__slideInLeft animate__slower animate__infinite" >집에서 편하게 <br></br>운동하세요,<br></br> JYGY가 <br></br>정확한 운동량을 카운트 해드립니다</div>
+        <div className="copy animate__animated animate__slideInLeft animate__slower animate__infinite" style={{color:'#5F5A64'}}>집에서 편하게 <br></br>운동하세요,<br></br> JYGY가 <br></br>정확한 운동량을 카운트 해드립니다</div>
         </div>
       </div>
 
     <div className="">
-      <p className="desc animate__animated animate__fadeIn animate__slower animate__delay-4s animate__infinite">
+      <p className="desc animate__animated animate__fadeIn animate__slower animate__delay-4s animate__infinite" style={{color:'#5F5A64'}}>
         JYGY와 함께한 건강한 생활을 <br></br>한눈에 확인하세요
       </p>
 
@@ -63,7 +63,7 @@ export default function Parallax() {
       </div>
 
       <div className="">
-        <p className="desc2 animate__animated animate__fadeIn animate__slower animate__delay-4s animate__infinite">
+        <p className="desc2 animate__animated animate__fadeIn animate__slower animate__delay-4s animate__infinite" style={{color:'#5F5A64'}}>
           여기서 끝이 아닙니다.<br></br>
           금융 혜택이 <br></br>당신을 기다리고 있어요!
         </p>
